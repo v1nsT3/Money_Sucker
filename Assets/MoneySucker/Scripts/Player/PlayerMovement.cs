683d1c94099494d28ef317556191975fc60a3469
v1nsT3 <105610480+v1nsT3@users.noreply.github.com>
2022-09-11 18:28:34 +0500

    rename vacuum

--- Assets/MoneySucker/Scripts/Player/PlayerMovement.cs
+++ Assets/MoneySucker/Scripts/Player/PlayerMovement.cs
@@ -6,18 +6,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Joystick _joystick;
     [SerializeField] private float _speedMove;
     [SerializeField] private float _speedRotate;
-    [SerializeField] private float _maxDistance;
-
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private Animator _animator;
-    private int _moveFloat = Animator.StringToHash("IsMove");
-    private Vector3 _targetVelocity;
 
-    private void Start()
-    {
-        //_rigidbody = GetComponent<Rigidbody>();
-        //_animator = GetComponent<Animator>();
-    }
+    private readonly int _moveFloat = Animator.StringToHash("IsMove");
 
     private void Update()
     {
@@ -28,16 +20,9 @@ public class PlayerMovement : MonoBehaviour
     {
         _rigidbody.velocity = new Vector3(_joystick.Horizontal * _speedMove, _rigidbody.velocity.y, _joystick.Vertical * _speedMove);
 
-
         if (_joystick.Vertical != 0 || _joystick.Horizontal != 0)
         {
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(_rigidbody.velocity), _speedRotate * Time.fixedDeltaTime);
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(_targetVelocity, 0.5f);
     }
 }
